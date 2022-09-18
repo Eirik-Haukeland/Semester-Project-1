67b30b20c364db2523316942cda6feae03f3dcc1
Eirik Berget Haukeland <eirik@haukeland.name>
2022-09-18 16:10:25 +0200

    added js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const openMenuBtn = document.getElementById('open-menu-btn');
+const closeMenuBtn = document.getElementById('close-menu-btn')
+const aside = document.querySelector('aside');
+const main = document.querySelector('main')
+
+openMenuBtn.addEventListener('click', (event) => { 
+  aside.hidden = false;
+  //make main stopp scrolling neads to only scroll aside
+})
+
+closeMenuBtn.addEventListener('click', (event) => { 
+  aside.hidden = true;
+  //make main scroll again
+})
